42b871bf8c0e4e8e146bd4d79658498a48cd2f91
Tayyab Malik <tayyabmalik3470@gmail.com>
2026-08-16 17:55:09 +0500

    Add Day 7 Vulnerable Code

---
+++ day7/day7_vulnerable.js
@@ -0,0 +1,17 @@
+﻿const express = require('express');
+const app = express();
+
+app.get('/search', (req, res) => {
+    const query = req.query.q || '';
+    res.send(`
+        <html>
+            <body>
+                <h2>Search Results</h2>
+                <p>Query executed: <b>${query}</b></p>
+                <p style="color:red;">[VULNERABILITY PROOF: Reflected XSS Payload Triggered Successfully]</p>
+            </body>
+        </html>
+    `);
+});
+
+app.listen(3000, () => console.log('Day 7 Vulnerable Server running on http://localhost:3000'));
